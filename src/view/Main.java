c27cf8dcddaa30b64765cf4adb624ac99a774db6
VoodLyc <j.sebasgr036@gmail.com>
2019-11-24 22:13:13 -0500

    TWICE「STAY BY MY SIDE」Making Music Video :microphone:  :headphones:

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,6 +1,9 @@
 package view;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -14,7 +17,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		
 		try {
-
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/LogIn.fxml"));
+			Parent root = (Parent) loader.load();
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setTitle("TWICE");
+			primaryStage.show();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
